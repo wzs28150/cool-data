d1a94ebd0b2dff8e7bffaa48f39a5b1f2b9536ef
wzs28150 <wz666s@163.com>
2022-04-11 10:49:51 +0800

    修改命令下载地址

--- src/init-template.js
+++ src/init-template.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
+const cp = require("child_process");
 const join = path.join;
 const basename = path.basename;
 const fs = require("fs");
@@ -28,7 +29,7 @@ function init(args) {
   console.log(cooldatavCli + (!isCn ? " is booting... " : " 正在启动..."));
   console.log(
     cooldatavCli +
-    (!isCn ? " will execute init command... " : " 即将执行 init 命令...")
+      (!isCn ? " will execute init command... " : " 即将执行 init 命令...")
   );
   checkAppName(projectName);
   if (existsSync(dest) && !emptyDir.sync(dest)) {
@@ -42,295 +43,73 @@ function init(args) {
   function createApp() {
     console.log(
       chalk.bold.cyan("cooldatav CLI") +
-      (!isCn ?
-        " will creating a new cooldatav app in " :
-        " 即将创建一个新的应用在 ") +
-      dest
+        (!isCn
+          ? " will creating a new cooldatav app in "
+          : " 即将创建一个新的应用在 ") +
+        dest
     );
     let url = "";
     switch (templateName) {
       case "vue3":
-        url = "http://114.115.177.23:8099/root/cooldatav-weapp.git";
-        weapp(url);
+        url = "https://gitee.com/harbin_kuchuang_network_wz666s/cool-data.git";
+        down(url, 'vue3-project');
         break;
       case "vue2":
-        url = "http://114.115.177.23:8099/root/pc-m.git";
-        pcm(url);
+        url = "https://gitee.com/harbin_kuchuang_network_wz666s/cool-data.git";
+        down(url, 'vue2-project');
         break;
       case "c-vue3":
-        url = "http://114.115.177.23:8099/root/pc-s.git";
-        weapp(url);
+        url = "https://gitee.com/harbin_kuchuang_network_wz666s/cool-data.git";
+        down(url, 'main');
         break;
-      case "c-vue3":
-        url = "https://github.com/wzs28150/cool-vue-vite-cli.git";
-        weapp(url);
+      case "c-vue2":
+        url = "https://gitee.com/harbin_kuchuang_network_wz666s/cool-data.git";
+        down(url, 'vue2');
         break;
     }
   }
 
-  function weapp(url) {
-    const {
-      status,
-      error: cloneError
-    } = spawn.sync("git", [
-      "clone",
-      "--depth=1",
-      url,
-      customPrjName || ".",
-    ]);
-    // verify git clone succeed
-    if (!cloneError && status === 0) {
-      try {
-        try {
-          // change a project name if project.config.json exist
-          if (existsSync(join(dest, "project.config.json"))) {
-            // eslint-disable-next-line
-            const appProjectJson = require(join(dest, "project.config.json"));
-            appProjectJson.projectname = projectName;
-            fs.writeFile(
-              join(dest, "project.config.json"),
-              JSON.stringify(appProjectJson, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-          }
-          // change a package name as a project name if package.json exist
-          if (existsSync(join(dest, "package.json"))) {
-            // eslint-disable-next-line
-            const appPackage = require(join(dest, "package.json"));
-            appPackage.name = projectName;
-            fs.writeFile(
-              join(dest, "package.json"),
-              JSON.stringify(appPackage, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-            process.chdir(customPrjName || ".");
+  function down(url, origin) {
+    info("正在下载项目文件。。。", "请不要退出！！！");
+    cp.exec(
+      `git clone -b ${origin} ${url} ${customPrjName || "."}`,
+      (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
+        if (error) {
+          // 当有错误时打印出错误并退出操作
+          error(`安装失败请重试!`);
+        } else {
+          try {
+            if (existsSync(join(dest, "package.json"))) {
+              const appPackage = require(join(dest, "package.json"));
+              appPackage.name = projectName;
+              fs.writeFile(
+                join(dest, "package.json"),
+                JSON.stringify(appPackage, null, 2),
+                (err) => {
+                  if (err) return console.log(err);
+                }
+              );
+              process.chdir(customPrjName || ".");
 
-            // install ndoe package modules
-            info("正在安装依赖", "你可以使用 ctrl + c 退出，然后自行安装。");
-            console.log();
-            require("./install")(mirror, done); // npm install
-          } else {
-            done();
-          }
-          // remove .git
-          const gitPath = join(dest, ".git");
-          if (existsSync(gitPath)) {
-            // spawn.sync("rm", ["-rf", gitPath]);
-            delDir(gitPath);
-          }
-        } catch (e) {
-          console.log(error(e));
+              // install ndoe package modules
+              info("正在安装依赖", "你可以使用 ctrl + c 退出，然后自行安装。");
+              console.log();
+              require("./install")(mirror, done); // npm install
+            } else {
+              done();
+            }
+            const gitPath = join(dest, ".git");
+            if (existsSync(gitPath)) {
+              // spawn.sync("rm", ["-rf", gitPath]);
+              delDir(gitPath);
+            }
+          } catch (error) {}
         }
-      } catch (e) {
-        console.log(error(e));
       }
-    } else {
-      // if incorrect template name
-      error(`安装失败请重试!`);
-    }
+    );
   }
 
-  function tp5(url) {
-    const {
-      status,
-      error: cloneError
-    } = spawn.sync("git", [
-      "clone",
-      "--depth=1",
-      url,
-      customPrjName || ".",
-    ]);
-    // verify git clone succeed
-    if (!cloneError && status === 0) {
-      try {
-        try {
-          // change a project name if project.config.json exist
-          if (existsSync(join(dest, "project.config.json"))) {
-            // eslint-disable-next-line
-            const appProjectJson = require(join(dest, "project.config.json"));
-            appProjectJson.projectname = projectName;
-            fs.writeFile(
-              join(dest, "project.config.json"),
-              JSON.stringify(appProjectJson, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-          }
-          // change a package name as a project name if package.json exist
-          if (existsSync(join(dest, "package.json"))) {
-            // eslint-disable-next-line
-            const appPackage = require(join(dest, "package.json"));
-            appPackage.name = projectName;
-            fs.writeFile(
-              join(dest, "package.json"),
-              JSON.stringify(appPackage, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-            process.chdir(customPrjName || ".");
-
-            // install ndoe package modules
-            info("正在安装依赖", "你可以使用 ctrl + c 退出，然后自行安装。");
-            console.log();
-            require("./install")(mirror, done); // npm install
-          } else {
-            tp5done();
-          }
-          // remove .git
-          const gitPath = join(dest, ".git");
-          if (existsSync(gitPath)) {
-            // spawn.sync("rm", ["-rf", gitPath]);
-            delDir(gitPath);
-          }
-        } catch (e) {
-          console.log(error(e));
-        }
-      } catch (e) {
-        console.log(error(e));
-      }
-    } else {
-      // if incorrect template name
-      error(`安装失败请重试!`);
-    }
-  }
-
-  function pcm(url) {
-    const {
-      status,
-      error: cloneError
-    } = spawn.sync("git", [
-      "clone",
-      "--depth=1",
-      url,
-      customPrjName || ".",
-    ]);
-    // verify git clone succeed
-    if (!cloneError && status === 0) {
-      try {
-        try {
-          // change a project name if project.config.json exist
-          if (existsSync(join(dest, "project.config.json"))) {
-            // eslint-disable-next-line
-            const appProjectJson = require(join(dest, "project.config.json"));
-            appProjectJson.projectname = projectName;
-            fs.writeFile(
-              join(dest, "project.config.json"),
-              JSON.stringify(appProjectJson, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-          }
-          // change a package name as a project name if package.json exist
-          if (existsSync(join(dest, "package.json"))) {
-            // eslint-disable-next-line
-            const appPackage = require(join(dest, "package.json"));
-            appPackage.name = projectName;
-            fs.writeFile(
-              join(dest, "package.json"),
-              JSON.stringify(appPackage, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-            process.chdir(customPrjName || ".");
-
-            // install ndoe package modules
-            info("正在安装依赖", "你可以使用 ctrl + c 退出，然后自行安装。");
-            console.log();
-            require("./install")(mirror, done); // npm install
-          } else {
-            pcmdone();
-          }
-          // remove .git
-          const gitPath = join(dest, ".git");
-          if (existsSync(gitPath)) {
-            // spawn.sync("rm", ["-rf", gitPath]);
-            delDir(gitPath);
-          }
-        } catch (e) {
-          console.log(error(e));
-        }
-      } catch (e) {
-        console.log(error(e));
-      }
-    } else {
-      // if incorrect template name
-      error(`安装失败请重试!`);
-    }
-  }
-
-  function wapm(url) {
-    const {
-      status,
-      error: cloneError
-    } = spawn.sync("git", [
-      "clone",
-      "--depth=1",
-      url,
-      customPrjName || ".",
-    ]);
-    // verify git clone succeed
-    if (!cloneError && status === 0) {
-      try {
-        try {
-          // change a project name if project.config.json exist
-          if (existsSync(join(dest, "project.config.json"))) {
-            // eslint-disable-next-line
-            const appProjectJson = require(join(dest, "project.config.json"));
-            appProjectJson.projectname = projectName;
-            fs.writeFile(
-              join(dest, "project.config.json"),
-              JSON.stringify(appProjectJson, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-          }
-          // change a package name as a project name if package.json exist
-          if (existsSync(join(dest, "package.json"))) {
-            // eslint-disable-next-line
-            const appPackage = require(join(dest, "package.json"));
-            appPackage.name = projectName;
-            fs.writeFile(
-              join(dest, "package.json"),
-              JSON.stringify(appPackage, null, 2),
-              (err) => {
-                if (err) return console.log(err);
-              }
-            );
-            process.chdir(customPrjName || ".");
-
-            // install ndoe package modules
-            info("正在安装依赖", "你可以使用 ctrl + c 退出，然后自行安装。");
-            console.log();
-            require("./install")(mirror, wapmdone); // npm install
-          } else {
-            wapmdone();
-          }
-          // remove .git
-          const gitPath = join(dest, ".git");
-          if (existsSync(gitPath)) {
-            // spawn.sync("rm", ["-rf", gitPath]);
-            delDir(gitPath);
-          }
-        } catch (e) {
-          console.log(error(e));
-        }
-      } catch (e) {
-        console.log(error(e));
-      }
-    } else {
-      // if incorrect template name
-      error(`安装失败请重试!`);
-    }
-  }
 
 
   function delDir(p) {
@@ -358,12 +137,16 @@ function init(args) {
     console.log();
     success(`跳转目录: cd ${projectName}`);
     console.log();
-    success("生成页面: cooldatav -p [name]");
+    success("运行项目: npm run dev");
     console.log();
-    success("生成组件: cooldatav -c [name]");
+    success("运行项目: npm run serve(同时启动案例及文档)");
     console.log();
-    success("生成接口: cooldatav -a [name]");
-    console.log();
+    // success("生成页面: cooldatav -p [name]");
+    // console.log();
+    // success("生成组件: cooldatav -c [name]");
+    // console.log();
+    // success("生成接口: cooldatav -a [name]");
+    // console.log();
     success(`恭喜你! "${projectName}"项目初始化成功! `);
     console.log();
     console.log();
@@ -399,13 +182,12 @@ function init(args) {
     console.log();
   }
 
-
   function wapmdone() {
     console.log();
     success(`跳转目录: cd ${projectName}`);
     console.log();
-    const child = spawn('cd', [`cd ${projectName}`], {
-      stdio: 'inherit'
+    const child = spawn("cd", [`cd ${projectName}`], {
+      stdio: "inherit",
     });
     success("初始化页面: npm run init(请先配置page.js)");
     console.log();
@@ -417,10 +199,6 @@ function init(args) {
     console.log();
     console.log();
   }
-
-
-
-
 }
 
 module.exports = init;
