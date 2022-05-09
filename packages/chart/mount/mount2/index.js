00fce6629d787edb25f0c90192d4b4fbd8f7b3d2
wzs28150 <wz666s@163.com>
2022-05-09 08:44:10 +0800

    add

--- packages/chart/mount/mount2/index.js
+++ packages/chart/mount/mount2/index.js
@@ -1,7 +1,7 @@
 import Mount2 from "./index.vue";
 import defaultOption from "./config";
 // eslint-disable-next-line func-names
-Mount2.title = "圆角山峰图";
+Mount2.title = "三角山峰图";
 Mount2.name = "Mount2";
 Mount2.type = "mount";
 Mount2.config = defaultOption;
--- packages/chart/mount/mount3/index.js
+++ packages/chart/mount/mount3/index.js
@@ -1,7 +1,7 @@
 import Mount3 from "./index.vue";
 import defaultOption from "./config";
 // eslint-disable-next-line func-names
-Mount3.title = "三角山峰图";
+Mount3.title = "园角山峰图";
 Mount3.name = "Mount3";
 Mount3.type = "mount";
 Mount3.config = defaultOption;
