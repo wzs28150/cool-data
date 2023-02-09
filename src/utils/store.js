e5c5179beb43bf6065fc17e73710046b5df6c214
wzs <1003418012@qq.com>
2023-02-09 16:56:46 +0800

    安装esc打包插件

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,45 +1,44 @@
 /*
- * @Title: 
- * @Descripttion: 
- * @version: 
+ * @Title:
+ * @Descripttion:
+ * @version:
  * @Author: wzs
  * @Date: 2022-04-10 12:30:52
  * @LastEditors: wzs
- * @LastEditTime: 2022-04-10 16:21:51
+ * @LastEditTime: 2023-02-09 16:45:13
  */
-import * as path from "path";
+import path from 'path-browserify';
 /**
  * 自动导入 Store
  */
- export function importAllStore () {
+export function importAllStore() {
   const modules = {};
   try {
-      // 导入 @/views 下文件，包含子目录，文件名为：store.js
-      const viewsRequireModules = import.meta.globEager('../views/**/store.js');
-      for (const path in viewsRequireModules) {
-          const modulesConent = viewsRequireModules[path];
-          if(modulesConent.default) {
-            const { name, ...module } = modulesConent.default; 
-            // 获取 PascalCase 命名
-            const modulesName = name || path.replace(/^\.\/(.*)\.\w+$/, "$1");
+    // 导入 @/views 下文件，包含子目录，文件名为：store.js
+    const viewsRequireModules = import.meta.globEager('../views/**/store.js');
+    for (const path in viewsRequireModules) {
+      const modulesConent = viewsRequireModules[path];
+      if (modulesConent.default) {
+        const { name, ...module } = modulesConent.default;
+        // 获取 PascalCase 命名
+        const modulesName = name || path.replace(/^\.\/(.*)\.\w+$/, '$1');
 
-            modules[modulesName] = { ...module }; 
-          }
+        modules[modulesName] = { ...module };
       }
-      
-      // 导入 @/store 下文件 
-      const requireModules = import.meta.globEager('../store/*.js');
-      for (const path in requireModules) {
-          const modulesConent = requireModules[path];
-          if(modulesConent.default) {
-            const { name, ...module } = modulesConent.default; 
-            // 获取 PascalCase 命名
-            const modulesName = name || path.replace(/^\.\/(.*)\.\w+$/, "$1");
+    }
 
-            modules[modulesName] = { ...module }; 
-          }
+    // 导入 @/store 下文件
+    const requireModules = import.meta.globEager('../store/*.js');
+    for (const path in requireModules) {
+      const modulesConent = requireModules[path];
+      if (modulesConent.default) {
+        const { name, ...module } = modulesConent.default;
+        // 获取 PascalCase 命名
+        const modulesName = name || path.replace(/^\.\/(.*)\.\w+$/, '$1');
+
+        modules[modulesName] = { ...module };
       }
-
+    }
   } catch (error) {
     console.log(error);
   }
@@ -50,8 +49,7 @@ import * as path from "path";
 /**
  * 验证 Store 位置
  */
- export function validateStorePath(filePath) {
-
+export function validateStorePath(filePath) {
   let storeDirBool = false;
   let viewsDirBool = false;
 
@@ -61,42 +59,36 @@ import * as path from "path";
   storeDirBool = filePath.replace(storePath, '') !== filePath;
 
   const filePathViews = filePath.replace(viewsPath, '');
-  if(filePathViews !== filePath) {
+  if (filePathViews !== filePath) {
     viewsDirBool = filePathViews.replace('store.js', '') !== filePathViews;
   }
- 
+
   // console.log('....validateStorePath....', storeDirBool, viewsDirBool);
-  
+
   return storeDirBool || viewsDirBool;
 }
 /**
  * Stroe Vite Plugin
  */
- export function vitePluginStore() {
-
+export function vitePluginStore() {
   return {
     name: 'vite-plugin-store',
-    configureServer(server) { 
-      
-        server.watcher.on("all",(eventName, filePath)=> {
-          
-          if ((eventName === 'add' || eventName === 'unlink')) {
+    configureServer(server) {
+      server.watcher.on('all', (eventName, filePath) => {
+        if (eventName === 'add' || eventName === 'unlink') {
+          if (validateStorePath(filePath)) {
+            server.moduleGraph.invalidateAll();
 
-            if(validateStorePath(filePath)) {
-              server.moduleGraph.invalidateAll();
+            server.ws.send({
+              type: 'full-reload',
+              path: '*',
+            });
 
-              server.ws.send({
-                type: 'full-reload',
-                path: '*'
-              });
-
-              server.config.logger.info(`  >>> ${eventName} ${filePath}`);            
-              server.config.logger.info(`  >>> moduleGraph invalidateAll, page reload all.`); 
-            } 
-
+            server.config.logger.info(`  >>> ${eventName} ${filePath}`);
+            server.config.logger.info(`  >>> moduleGraph invalidateAll, page reload all.`);
           }
-        })
-    }
-  }
-
+        }
+      });
+    },
+  };
 }
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import legacy from "@vitejs/plugin-legacy";
-import { setNodeEnv } from "./src/utils/env";
-import { vitePluginMock } from "./src/utils/mock";
-import { vitePluginStore } from "./src/utils/store";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
+import { setNodeEnv } from './src/utils/env';
+import { vitePluginMock } from './src/utils/mock';
+import { vitePluginStore } from './src/utils/store';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { zip, progress } from 'esc-plugin';
 export default defineConfig(({ mode, command }) => {
   setNodeEnv(mode);
-  const port = parseInt(process.env.APP_PORT || "3000");
+  const port = parseInt(process.env.APP_PORT || '3000');
 
   const plugins = [
     vue(),
@@ -22,6 +22,13 @@ export default defineConfig(({ mode, command }) => {
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    progress(),
+    zip({
+      type: 'zip',
+      archiverName: 'dist.zip',
+      sourceName: 'dist',
+      open: true,
+    }),
     // legacy({
     //   targets: ['ie >= 11'],
     //   additionalLegacyPolyfills: ['regenerator-runtime/runtime']
@@ -29,37 +36,50 @@ export default defineConfig(({ mode, command }) => {
   ];
 
   // @vitejs/plugin-legacy
-  command === "build" && plugins.push(legacy());
-  mode === "development" &&
-  process.env.APP_MOCK === "true" &&
-  plugins.push(vitePluginMock());
+  command === 'build' && plugins.push(legacy());
+  mode === 'development' && process.env.APP_MOCK === 'true' && plugins.push(vitePluginMock());
 
   return {
-    base: "./",
+    base: './',
     plugins,
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
       port,
-      host: "0.0.0.0",
-      open: true
+      host: '0.0.0.0',
+      open: true,
     },
     define: {
       // 定义全局常量
     },
     optimizeDeps: {
-      include: [
-        "axios",
-        "mockjs",
-        "vue",
-        "vuex",
-        "vuex-module-decorators",
-        "element-plus"
-      ],
+      include: ['axios', 'mockjs', 'vue', 'vuex', 'vuex-module-decorators', 'element-plus'],
       exclude: [],
+    },
+    build: {
+      assetsDir: 'static',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: (assetInfo) => {
+            var info = assetInfo.name.split('.');
+            var extType = info[info.length - 1];
+            if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = 'media';
+            } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = 'img';
+            } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = 'fonts';
+            }
+            // console.log(assetInfo.name)
+            return `static/${extType}/[name]-[hash][extname]`;
+          },
+        },
+      },
     },
   };
 });
